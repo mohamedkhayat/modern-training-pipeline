7ee0a24f1eff6e4241635e9efbc7ef16c555d5e7
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2025-06-06 19:31:02 +0100

    made language around the metric a bit more general

--- src/early_stop.py
+++ src/early_stop.py
@@ -12,18 +12,18 @@ class EarlyStopping:
         self.name = name
         self.full_path = path / f"{name}_best.pth"
 
-        self.best_acc = None
+        self.best_metric = None
         self.counter = 0
         self.earlystop = False
 
-    def __call__(self, val_acc, model):
-        if self.best_acc is None:
-            self.best_acc = val_acc
+    def __call__(self, val_metric, model):
+        if self.best_metric is None:
+            self.best_metric = val_metric
 
-        elif val_acc <= self.best_acc + self.delta:
+        elif val_metric <= self.best_metric + self.delta:
             self.counter += 1
         else:
-            self.best_acc = val_acc
+            self.best_metric = val_metric
             torch.save(model.state_dict(), self.full_path)
             self.counter = 0
             print("saved model weights")
