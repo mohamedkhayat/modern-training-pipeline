cbef5389351064ef1ec9d5e773ffa80f47bf53c1
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2025-06-08 16:12:15 +0100

    experimenting with this

--- src/models/model_avgpool.py
+++ src/models/model_avgpool.py
@@ -1,11 +1,10 @@
 import torch.nn as nn
 from .convBlock import ConvBlock
 
-
 class CNN_AVG_POOL(nn.Module):
-    def __init__(self, out_size=9, last_filter_size=128):
+    def __init__(self,out_size=9, last_filter_size=128):
         super(CNN_AVG_POOL, self).__init__()
-
+        """
         self.blocks = nn.ModuleList(
             [
                 ConvBlock(
@@ -52,23 +51,57 @@ class CNN_AVG_POOL(nn.Module):
                 # 128 x 3 x 3
             ]
         )
+        """
+        self.blocks = nn.ModuleList(
+            [
+                ConvBlock(
+                    in_out_shapes=[3, 64],
+                    kernels=[7, 3],
+                    strides=[2, 1],
+                    paddings=[3, 1],
+                ),
+
+                # 32 x 56 x 56
+                ConvBlock(
+                    in_out_shapes=[64, 128],
+                    kernels=[5, 3],
+                    strides=[2, 1],
+                    paddings=[2, 1],
+                ),
+
+                # 64 x 14 x 14
+                ConvBlock(
+                    in_out_shapes=[128, 256],
+                    kernels=[3, 3],
+                    strides=[2, 1],
+                    paddings=[1, 1],
+                ),
+                ConvBlock(
+                    in_out_shapes=[256, last_filter_size],
+                    kernels=[3, 3],
+                    strides=[2, 1],
+                    paddings=[1, 1],
+                ),
+
+            ]
+        )
         # shape after this block : 128 x 3 x 3
         self.convblocks = nn.Sequential(*self.blocks)
 
         self.gavgpool = nn.AdaptiveAvgPool2d(1)
-        self.fc1 = nn.Linear(last_filter_size, out_size)
+        self.fc1 = nn.Linear(last_filter_size,out_size)
         self.apply(self._init_weights)
 
     def _init_weights(self, m):
         if isinstance(m, nn.Conv2d):
-            nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity='relu')
             if m.bias is not None:
                 nn.init.constant_(m.bias, 0)
 
         elif isinstance(m, nn.BatchNorm1d) or isinstance(m, nn.BatchNorm2d):
             nn.init.constant_(m.weight, 1)
             nn.init.constant_(m.bias, 0)
-
+            
         elif isinstance(m, nn.Linear):
             nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
             if m.bias is not None:
