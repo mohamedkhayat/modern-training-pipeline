cd4391199d0beb2fb62daa460ced54d10eefe106
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2025-06-08 16:03:53 +0100

    added logging transform visualizations, now stratify train test split instead of just blind split, added support for easy and hard augs, added F1 and Accuracy, eval returns y_true and y_pred for confusion matrix logging, and adding a function to log confusion matrix

--- src/utils/generalutils.py
+++ src/utils/generalutils.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, random_split
+from sklearn.model_selection import train_test_split
 from dataset import FishDataset
 from typing import Dict
 from torch import autocast
@@ -9,13 +10,15 @@ from torch.amp import GradScaler
 from datetime import datetime
 import wandb
 from omegaconf import OmegaConf
-from torchmetrics.classification import F1Score
+from torchmetrics.classification import F1Score, Accuracy
 import random
 import numpy as np
 import functools
 from pathlib import Path
 import os
-
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def get_data_samples(root_dir):
     root_dir = Path(root_dir)
@@ -31,6 +34,38 @@ def get_data_samples(root_dir):
 
     return samples, classes, class_to_idx
 
+def unnormalize(img_tensor, mean, std):
+    # mean (1, 3) 
+    mean = torch.tensor(mean).view(-1, 1, 1)
+    # std (1, 3) 
+    std = torch.tensor(std).view(-1, 1, 1)
+
+    return img_tensor * std + mean
+
+def log_transforms(run, batch, n_images, classes, aug, mean, std):
+    cols = 3
+    rows = (n_images + cols - 1) // cols
+    
+    fig, axes = plt.subplots(rows, cols, figsize=(rows * 3, cols * 3))
+    axes = axes.flatten()
+    
+    images, labels = batch
+    fig.suptitle(f"{aug} transforms", fontsize=16)
+
+    for ax, img, label in zip(axes, images[:n_images], labels[:n_images]):
+        # 1 x 3 x H x W
+        img = img.squeeze(0)
+        img = unnormalize(img, mean, std).cpu().numpy()
+        img = img.transpose(1,2,0)
+        img = (img * 255).clip(0, 255).astype("uint8")
+
+        ax.imshow(img)
+        ax.axis('off')
+        ax.set_title(f"{classes[label]}") 
+
+    plt.tight_layout()
+    run.log({"transforms visualization": wandb.Image(fig)})
+    plt.close(fig)
 
 def set_seed(SEED):
     torch.manual_seed(SEED)
@@ -44,12 +79,12 @@ def set_seed(SEED):
 def make_dataset(
     root_dir: str,
     train_ratio: float,
-    generator: torch.Generator,
+    SEED : int,
     get_stats: bool = False,
 ):
     print("... making dataset ...")
     data_samples, classes, class_to_idx = get_data_samples(root_dir)
-
+    """
     dataset_size = len(data_samples)
     train_size = int(train_ratio * dataset_size)
     test_size = dataset_size - train_size
@@ -57,16 +92,19 @@ def make_dataset(
     train_set, test_set = random_split(
         data_samples, [train_size, test_size], generator=generator
     )
+    """
 
+    train_set, test_set = train_test_split(data_samples, train_size=train_ratio, stratify = [label for _, label in data_samples], random_state = SEED)
+    
     train_ds = FishDataset(train_set, classes, class_to_idx)
     test_ds = FishDataset(test_set, classes, class_to_idx)
 
     mean, std = None, None
 
     if get_stats:
-        mean, std = train_ds.compute_mean_std()
+        mean, std = train_ds.get_mean_std()
 
-    print("done")
+    print("... done ...")
     return train_ds, test_ds, mean, std
 
 
@@ -78,7 +116,7 @@ def seed_worker(worker_id, base_seed):
 
 
 def make_data_loaders(
-    train_ds, test_ds, transforms, batch_size: int, generator: torch.Generator
+    train_ds, test_ds, transforms, batch_size: int, generator: torch.Generator, aug:str
 ):
     """Creates training and testing data loaders from a dataset.
 
@@ -93,7 +131,7 @@ def make_data_loaders(
 
     base_seed = generator.initial_seed()
 
-    train_ds.transforms = transforms["train"]
+    train_ds.transforms = transforms[f"train_{aug}"]
     test_ds.transforms = transforms["test"]
 
     worker_init = functools.partial(seed_worker, base_seed=base_seed)
@@ -122,7 +160,7 @@ def make_data_loaders(
         generator=generator,
     )
 
-    print("done")
+    print("... done ...")
     return train_dl, test_dl
 
 
@@ -147,7 +185,10 @@ def train(
         scaler (GradScaler): Gradient scaler for mixed precision training.
     """
     loss = 0.0
-    metric = F1Score("multiclass", num_classes=num_classes, average="weighted").to(
+    f1 = F1Score("multiclass", num_classes=num_classes, average="weighted").to(
+        device
+    )
+    acc = Accuracy(task="multiclass", num_classes=num_classes, average="weighted").to(
         device
     )
 
@@ -167,15 +208,17 @@ def train(
         scaler.step(optimizer)
         scaler.update()
 
-        metric.update(out, y)
+        f1.update(out, y)
+        acc.update(out, y)
         loss += batch_loss.item()
 
-    f1_score = metric.compute()
+    f1_score = f1.compute()
+    accuracy = acc.compute()
     loss = loss / len(train_dl)
 
     print(f"Train - F1-Score : {f1_score:.4f} - Loss : {loss:.4f}")
 
-    return loss, f1_score
+    return loss, f1_score, accuracy
 
 
 @torch.no_grad()
@@ -197,8 +240,14 @@ def evaluate(
 
     """
 
+    y_true = []
+    y_pred = []
+
     loss = 0.0
-    metric = F1Score("multiclass", num_classes=num_classes, average="weighted").to(
+    f1 = F1Score("multiclass", num_classes=num_classes, average="weighted").to(
+        device
+    )
+    acc = Accuracy(task="multiclass", num_classes=num_classes, average="weighted").to(
         device
     )
 
@@ -214,13 +263,19 @@ def evaluate(
             batch_loss = criterion(out, y).item()
 
         loss += batch_loss
-        metric.update(out, y)
+        f1.update(out, y)
+        acc.update(out, y)
 
-    f1_score = metric.compute()
+        y_true.extend(y.cpu().numpy())
+        y_pred.extend(np.argmax(out.cpu().numpy(), axis = 1))
+
+    f1_score = f1.compute()
+    accuracy = acc.compute()
+
     loss = loss / len(test_dl)
     print(f"Eval - F1-Score : {f1_score:.4f} - Loss : {loss:.4f}")
-
-    return loss, f1_score
+    
+    return loss, f1_score, accuracy, y_true, y_pred
 
 
 def get_run_name(cfg):
@@ -240,3 +295,19 @@ def initwandb(cfg):
         config=OmegaConf.to_container(cfg, resolve=True),
     )
     return run
+
+def log_confusion_matrix(run,y_true, y_pred, classes):
+    cm = confusion_matrix(y_true, y_pred)
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues",
+                xticklabels=classes, yticklabels=classes, ax=ax)
+    ax.set_title("Confusion Matrix")
+    ax.set_xlabel("Predicted Labels")
+    ax.set_ylabel("True Labels")
+    plt.tight_layout()
+    
+    run.log({"confusion_matrix": wandb.Image(fig)})
+    plt.close(fig)
+
+    
