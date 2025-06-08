2ffc3514fc223b9ddc9e0a6af371ed3c5980a1e1
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2025-06-08 16:08:13 +0100

    formatted, made calls to the added logging methods and now log accuracy as well

--- src/main.py
+++ src/main.py
@@ -10,18 +10,20 @@ from utils.generalutils import (
     get_run_name,
     set_seed,
     make_dataset,
+    log_confusion_matrix,
+    log_transforms,
 )
 import torch.optim as optim
 from torch.amp import GradScaler
 from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts, SequentialLR, LinearLR
 import hydra
 from omegaconf import DictConfig
-from model_factory import get_model
+from models.model_factory import get_model
 
 
 @hydra.main(version_base=None, config_path="../conf", config_name="config")
 def main(cfg: DictConfig):
-    if cfg.logwandb:
+    if cfg.log:
         run = initwandb(cfg)
         name = run.name
     else:
@@ -30,25 +32,32 @@ def main(cfg: DictConfig):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"using : {device}")
 
-    generator = set_seed(42)
+    generator = set_seed(cfg.seed)
 
     root_dir = pathlib.Path(rf"{cfg.root_dir}")
 
     train_ds, test_ds, mean, std = make_dataset(
-        root_dir, cfg.train_ratio, generator, cfg.architecture in ["cnn_fc", "cnn_avg"]
+        root_dir,
+        cfg.train_ratio,
+        cfg.seed,
+        cfg.architecture in ["cnn_fc", "cnn_avg", "test"],
     )
 
     model, transforms = get_model(cfg, device, mean, std)
 
     train_dl, test_dl = make_data_loaders(
-        train_ds, test_ds, transforms, cfg.batch_size, generator
+        train_ds, test_ds, transforms, cfg.batch_size, generator, cfg.aug
     )
 
-    # TODO: switch to image segmentation
-    # TODO: add distillation
+    log_transforms(
+        run, next(iter(train_dl)), cfg.n_images, train_ds.classes, cfg.aug, mean, std
+    )
+    # TODO: switch dataset
+    # TODO: add cross validation
+    # TODO: add error analysis
 
-    optimizer = optim.AdamW(
-        filter(lambda p: p.requires_grad, model.parameters()),
+    optimizer = optim.Adam(
+        model.parameters(),
         lr=cfg.lr,
         weight_decay=cfg.weight_decay,
     )
@@ -56,9 +65,11 @@ def main(cfg: DictConfig):
     warmup_scheduler = LinearLR(
         optimizer, start_factor=0.1, total_iters=cfg.warmup_epochs
     )
+
     cosine_scheduler = CosineAnnealingWarmRestarts(
         optimizer, T_0=10, T_mult=2, eta_min=1e-6
     )
+
     scheduler = SequentialLR(
         optimizer,
         schedulers=[warmup_scheduler, cosine_scheduler],
@@ -72,28 +83,49 @@ def main(cfg: DictConfig):
     )
 
     best_val_f1 = 0.0
+    best_val_acc = 0.0
+
     for epoch in range(cfg.epochs):
         print(f"Epoch : {epoch + 1} Learning rate : {scheduler.get_last_lr()[0]:.5f}")
-        train_loss, train_f1 = train(
+
+        train_loss, train_f1, train_acc = train(
             model, device, train_dl, loss, cfg.model.out_size, optimizer, scaler
         )
-        val_loss, val_f1 = evaluate(model, device, test_dl, loss, cfg.model.out_size)
+        val_loss, val_f1, val_acc, _, _ = evaluate(
+            model, device, test_dl, loss, cfg.model.out_size
+        )
         best_val_f1 = max(val_f1, best_val_f1)
+        best_val_acc = max(val_acc, best_val_acc)
+
         scheduler.step()
-        if cfg.logwandb:
+
+        if cfg.log:
             run.log(
                 {
                     "train f1": train_f1,
                     "train loss": train_loss,
+                    "train acc": train_acc,
                     "val f1": val_f1,
                     "val loss": val_loss,
+                    "val acc": val_acc,
                     "Learning rate": float(f"{scheduler.get_last_lr()[0]:.6f}"),
                 }
             )
+
         if early_stopper(val_f1, model):
             break
-    if cfg.logwandb:
+
+    if cfg.log:
         run.log({"best val f1": best_val_f1})
+        run.log({"best val acc": best_val_acc})
+
+        model = early_stopper.get_best_model(model)
+        val_loss, val_f1, val_acc, y_true, y_pred = evaluate(
+            model, device, test_dl, loss, cfg.model.out_size
+        )
+
+        log_confusion_matrix(run, y_true, y_pred, train_ds.classes)
+
         run.finish()
 
 
