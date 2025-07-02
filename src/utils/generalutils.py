88e17de935212d5491039d81afa57458d92baa0c
mohamedkhayat <132056305+mohamedkhayat@users.noreply.github.com>
2025-07-02 12:30:50 +0100

    rafactor: move training and evaluation functions to seperate file

--- src/utils/generalutils.py
+++ src/utils/generalutils.py
@@ -1,22 +1,17 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from torch import autocast
 from torch.optim import Optimizer
-from torch.amp import GradScaler
 from omegaconf import DictConfig
-from torchmetrics.classification import F1Score, Accuracy
 import random
 import numpy as np
 import torch.optim as optim
-from captum.attr import LayerGradCam
 from torch.optim.lr_scheduler import (
     CosineAnnealingWarmRestarts,
     CosineAnnealingLR,
     SequentialLR,
     LinearLR,
 )
-
 from dataset import DS
 from utils.data_utils import get_class_weights
 
@@ -35,162 +30,6 @@ def seed_worker(worker_id, base_seed):
     np.random.seed(worker_seed)
     random.seed(worker_seed)
     torch.manual_seed(worker_seed)
-
-
-def train(
-    model: nn.Module,
-    device: torch.device,
-    train_dl: DataLoader,
-    criterion: nn.Module,
-    num_classes: int,
-    optimizer: Optimizer,
-    scaler: GradScaler,
-):
-    """Trains the model for one epoch using mixed precision.
-
-    Args:
-        model (nn.Module): Model to be trained.
-        device (torch.device): Device (CPU or GPU) to train on.
-        train_dl (DataLoader): Training data loader.
-        criterion (nn.Module): Loss function.
-        num_classes (int): Number of label classes to be used for F1 score
-        optimizer (Optimizer): Optimizer for updating model weights.
-        scaler (GradScaler): Gradient scaler for mixed precision training.
-    """
-    loss = 0.0
-    f1 = F1Score("multiclass", num_classes=num_classes, average="weighted").to(device)
-    acc = Accuracy(task="multiclass", num_classes=num_classes, average="weighted").to(
-        device
-    )
-
-    model.train()
-
-    for batch_idx, (x, y) in enumerate(train_dl):
-        optimizer.zero_grad(set_to_none=True)
-        x, y = x.to(device), y.to(device)
-
-        device_str = str(device).split(":")[0]
-
-        with autocast(device_type=device_str, dtype=torch.bfloat16):
-            out = model(x)
-            batch_loss = criterion(out, y)
-
-        scaler.scale(batch_loss).backward()
-
-        scaler.unscale_(optimizer)
-        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
-
-        scaler.step(optimizer)
-        scaler.update()
-
-        f1.update(out, y)
-        acc.update(out, y)
-        loss += batch_loss.item()
-
-    f1_score = f1.compute()
-    accuracy = acc.compute()
-
-    f1.reset()
-    acc.reset()
-
-    loss = loss / len(train_dl)
-
-    print(f"Train - F1-Score : {f1_score:.4f} - Loss : {loss:.4f}")
-
-    return loss, f1_score, accuracy
-
-
-def evaluate(
-    model: nn.Module,
-    device: torch.device,
-    test_dl: DataLoader,
-    criterion: nn.Module,
-    num_classes: int,
-    grad_cam: bool = False,
-):
-    """Evaluates the model on the test dataset.
-
-    Args:
-        model (nn.Module): Model to be evaluated.
-        device (torch.device): Device (CPU or GPU) to evaluate on.
-        test_dl (DataLoader): Test data loader.
-        criterion (nn.Module): Loss function.
-        num_classes (int): Number of label classes to be used for F1 score
-
-    """
-
-    y_true = []
-    y_pred = []
-
-    loss = 0.0
-    f1 = F1Score("multiclass", num_classes=num_classes, average="weighted").to(device)
-    acc = Accuracy(task="multiclass", num_classes=num_classes, average="weighted").to(
-        device
-    )
-
-    attributions = None
-
-    if grad_cam:
-        try:
-            imgs, _ = next(iter(test_dl))
-            input_tensor = imgs.to(device)
-
-            target_layer = get_last_conv(model)
-            print(f"Grad-CAM target layer: {target_layer}")
-
-            torch.set_grad_enabled(True)
-            model.zero_grad()
-
-            guided_gc = LayerGradCam(model, target_layer)
-
-            outputs = model(input_tensor)
-
-            targets = outputs.argmax(dim=1)
-            attributions = guided_gc.attribute(
-                input_tensor,
-                target=targets,
-                relu_attributions=True,
-            )
-            attributions = torch.nn.functional.interpolate(
-                attributions,
-                size=imgs.shape[2:],  # Target (H, W) of original image
-                mode="bilinear",
-                align_corners=False,
-            ).detach()  # Detach after interpolation
-
-            torch.set_grad_enabled(False)
-
-        except Exception as e:
-            print(f"Error computing attributions : {e}")
-
-    model.eval()
-    with torch.no_grad():
-        for x, y in test_dl:
-            x, y = x.to(device), y.to(device)
-
-            device_str = str(device).split(":")[0]
-
-            with autocast(device_type=device_str, dtype=torch.bfloat16):
-                out = model(x)
-                batch_loss = criterion(out, y).item()
-
-            loss += batch_loss
-            f1.update(out, y)
-            acc.update(out, y)
-
-            y_true.extend(y.cpu().numpy())
-            y_pred.extend(np.argmax(out.float().cpu().numpy(), axis=1))
-
-        f1_score = f1.compute()
-        accuracy = acc.compute()
-
-        f1.reset()
-        acc.reset()
-
-    loss = loss / len(test_dl)
-    print(f"Eval - F1-Score : {f1_score:.4f} - Loss : {loss:.4f}")
-
-    return loss, f1_score, accuracy, y_true, y_pred, attributions
 
 
 def get_optimizer(cfg: DictConfig, model: nn.Module) -> Optimizer:
